2578e6668dbc1c28d4b5e29566b3d75d231b3c15
VastCosmic <137017677@qq.com>
2022-11-06 10:35:02 +0800

    1.0

--- utils/global-data.js
+++ utils/global-data.js
@@ -7,7 +7,7 @@ export const getGlobalData = () => {
     : 'Perception of the Infinity.';
   const footerText = process.env.BLOG_FOOTER_TEXT
     ? decodeURI(process.env.BLOG_FOOTER_TEXT)
-    : 'All rights reserved by VastCosmic    桂ICP备2022009562号';
+    : "Created by VastCosmic \n 桂ICP备2022009562号 桂公网安备45090202000394号";
   return {
     name,
     blogTitle,
